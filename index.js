6199a9bd03a14db938a9c1c47362c25601ac0de1
Andrea <noerios880@gmail.com>
2024-04-16 22:38:53 -0600

    full

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParse = require("body-parser");
+const { ejemploInsertarDatos } = require('./database');
 
 const app = express();
 const PORT = process.env.PORT || 3977;
